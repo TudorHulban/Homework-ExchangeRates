ed346c212b6346316211976d91bd9892f24153ec
Tudor Hulban <prindex2010@gmail.com>
2021-04-14 15:30:46 +0300

    unit test for different day

--- t_model_test.go
+++ t_model_test.go
@@ -12,4 +12,8 @@ func TestAmount(t *testing.T) {
 	amount, errExch := e.ConvertedAmount(1, "RON", "2021-04-13")
 	require.Nil(t, errExch, "passed currency is not correct")
 	require.Equal(t, 4.9223, amount)
+
+	amount, errExch = e.ConvertedAmount(1, "RON", "2021-04-12")
+	require.Nil(t, errExch, "passed currency is not correct")
+	require.Equal(t, 4.9203, amount)
 }
